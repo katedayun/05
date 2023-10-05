10fb16031773727be138f97926e4a740b34e756a
katedayun <58518683+katedayun@users.noreply.github.com>
2023-10-05 15:59:45 +0900

    2

--- main.c
+++ main.c
@@ -4,18 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int integer;
+	int integer; 
+	int neginteger;
 	
-	printf("enter integer : ");
+	
+	printf("enter a integer:");
 	scanf("%d",&integer);
-	
-	if(integer>0)
-		printf("its an positive number\n");
-	
-	else if(integer<0)
-		printf("its an negtive number\n");
-	
-	else 
-		printf("its 0\n ");
+	if(integer>=0)
+		printf("absolute value is %d",integer);
+	else
+		{
+			printf("absolute value is %d",-integer);
+		}
 	return 0;
 }
