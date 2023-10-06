82c56f508ace394c73876f236a9b759f47f9c16f
katedayun <58518683+katedayun@users.noreply.github.com>
2023-10-07 06:06:30 +0900

    4

--- main.c
+++ main.c
@@ -4,19 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int num = 0;
-	 
-	printf("input a string: ");
+    int n;      // Variable to store the user input
+    int sum = 0; // Variable to store the sum, initialized to 0
+    int i;
 
-	while ((c=getchar())!='\n')
-	//'/n'enter 랑 같으면 그만! enter 랑 다르면 계속 한다고. 
-	//c=getchar() c =  문자를 저장한다.. 정수로 저장한다... 저장소의 차이.. get char = 문자를 하나 가져오는 함수 sook2018 '/n'하나씩 c 에 들어가.. 
-	//반복을 할때마다 하나씩 c 에 들어간다..  
-	{
-			if (c>='0'&&c<='9')
-				num=num+1;
-}
-printf("no.of digits is %i\n",num);
-	return 0;
+    // Prompt the user for input
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    // Calculate the sum from 1 to n
+    for (i = 1; i <= n; i++) 
+    {
+        sum += i;
+	}
+    
+
+    // Print the result
+    printf("The sum of numbers from 1 to %d is: %d\n", n, sum);
+
+    return 0;
 }
