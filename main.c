964b2cb6069c302ef3072348408a114c28fc39be
katedayun <58518683+katedayun@users.noreply.github.com>
2023-10-07 06:10:29 +0900

    6

--- main.c
+++ main.c
@@ -4,23 +4,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    int n;      // Variable to store the user input
-    int sum = 0; // Variable to store the sum, initialized to 0
-    int i;
-
-    // Prompt the user for input
-    printf("Enter a positive integer: ");
-    scanf("%d", &n);
-
-    // Calculate the sum from 1 to n
-    for (i = 1; i <= n; i++) 
-    {
-        sum += i;
-	}
-    
-
-    // Print the result
-    printf("The sum of numbers from 1 to %d is: %d\n", n, sum);
-
-    return 0;
+	int answer = 59;
+  	int x;
+  	int trial = 0;
+  
+  	do
+  	{
+      	printf("Guess a number : ");
+      	scanf("%d", &x);
+      
+                if(x > answer)
+                { printf("low!\n"); }
+                else if( x < answer)
+                {printf("high!\n"); } 
+      
+      trial = trial +1;
+    }
+  	while( x != answer);
+  
+  
+  printf("congratulation! trials : %i\n", trial);
+  return 0;
 }
